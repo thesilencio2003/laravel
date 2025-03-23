eb56ca2a8ae5bbcec4e632c525c72b44b41cb523
alejandro <alejohola24@gmail.com>
2025-03-23 13:27:35 -0500

    comuna list

---
+++ resources/views/comuna/index.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de comuna</title>
+  </head>
+  <body>
+    <div class="container">
+        <a href="{{ route('comuna.create') }}" class="btn btn-success" >add</a>
+        <h1>Listado de comunas</h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Code</th>
+      <th scope="col">Commune</th>
+      <th scope="col">Municipality</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach
+    <tr>
+      <th scope="row"> {{ $comuna->comu_codi }}</th>
+      <td> {{ $comuna->comu_nomb }}</td>
+      <td> {{ $comuna->muni_nomb }}</td>
+      <td> <span> Actions </span> </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+    </div>
+
+
+    
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
