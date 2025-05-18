fb0a33264c62a7e5e2b84e55734be7ec0e0d0aca
Alejandro <alejohola24@gmail.com>
2025-05-18 17:27:45 -0500

    primero cambios

---
+++ app/Http/Controllers/api/ComunaController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comuna;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ComunaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+            ->get();
+        return json_encode(['comunas' => $comunas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $validate = Validator::make($request->all(), [
+            'comu_nomb' => ['required', 'max:30', 'unique'],
+            'muni_codi' => ['required', 'numeric', 'min:1']
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'msg' => 'Se produjo un error en la validación de la información.',
+                'statusCode' => 400
+            ]);
+        }
+
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+        return json_encode(['comuna' => $comuna]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+
+        $comuna = Comuna::find($id);
+        if (is_null($comuna)) {
+            return abort(404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $comuna = Comuna::find($id);
+        $comuna->comu_nomb = $request->comu_nomb;
+        $comuna->muni_codi = $request->muni_codi;
+        $comuna->save();
+        return json_encode(['comuna' => $comuna]);
+
+        $comuna = Comuna::find($id);
+        if (is_null($comuna)) {
+            return abort(404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+        $comuna = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+        return json_encode(['comunas' => $comuna, 'success' => true]);
+
+        $comuna = Comuna::find($id);
+        if (is_null($comuna)) {
+            return abort(404);
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\ComunaController;
+use App\Http\Controllers\api\MunicipioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::get('/comunas/{comuna}', [ComunaController::class, 'show'])->name('comunas.show');
+Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+
+Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios');
